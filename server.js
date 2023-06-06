9ccdd0bc9710272d961cddffc606a827b8bc3ded
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-06-06 17:51:42 +0200

    werkomgeving geïnstalleerd
    Eerste opzet

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from 'express'
+
+// Maak een nieuwe express app
+const app = express()
+
+// Stel in hoe we express gebruiken
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
+
+
+// Maak een route voor de index
+app.get('/', (request, response) => {
+
+
+    fetchJson().then((data) => {
+        response.render('index')
+    })
+})
+
+
+
+
+
+
+
+// Stel het poortnummer in en start express
+app.set('port', process.env.PORT || 7000)
+app.listen(app.get('port'), function () {
+    console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+    return await fetch(url)
+        .then((response) => response.json())
+        .catch((error) => error)
+}
