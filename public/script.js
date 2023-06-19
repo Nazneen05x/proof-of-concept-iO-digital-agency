1db29a43acea560eb3cc2da6103811fc55c69120
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-06-19 23:43:38 +0200

    card animation

--- public/script.js
+++ public/script.js
@@ -16,3 +16,19 @@ function skillsscroll(){
     }
     )
 }
+
+// Scroll animation
+// bron: https://www.youtube.com/watch?v=T33NN_pPeNI
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+      console.log(entry)
+      if (entry.isIntersecting){
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      } 
+    });
+  });
+  
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
